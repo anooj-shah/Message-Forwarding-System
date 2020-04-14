a9771b7924e697655ab291a7dae1ba5ab8e52f47
Anooj Shah <thisisanooj@gmail.com>
2020-04-13 20:46:11 -0500

    mongo and twilio

---
+++ app.py
@@ -0,0 +1,58 @@
+from twilio.rest import Client
+import os
+import pymongo
+from flask import Flask
+
+app = Flask(__name__)
+
+# MongoDB
+DBClient = pymongo.MongoClient("mongodb+srv://anooj101:cmhtest@cmhclasses-s7bby.gcp.mongodb.net/test?retryWrites=true&w=majority")
+db = DBClient['cmh_classes_db']
+classes = db['classes']
+post = {'class':'Test', 'phone_numbers':['+18326006867']}
+classes.insert_one(post)
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+# message = client.messages.create(body='Hi there!', from_='+14783471874',to='+18326006867')
+#
+# print(message.sid)
+
+@app.route('/')
+def main():
+    return 'Hello, World!'
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    # Add a message
+    resp.message("The Robots are coming! Head for the hills!")
+
+    return str(resp)
+
+
+@app.route("/sms", methods=['GET', 'POST'])
+def incoming_sms():
+    """Send a dynamic reply to an incoming text message"""
+    # Get the message the user sent our Twilio number
+    body = request.values.get('Body', None)
+
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    # Determine the right reply for this message
+    if body == 'hello':
+        resp.message("Hi!")
+    elif body == 'bye':
+        resp.message("Goodbye")
+
+    return str(resp)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
